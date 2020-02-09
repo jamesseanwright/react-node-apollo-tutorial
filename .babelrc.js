77d9043935a4958fd328238e3192e02a994acccd
James Wright <james@jamesswright.co.uk>
2020-02-09 17:56:13 +0000

    Adds build and start scripts

---
+++ .babelrc.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = api => {
+  api.cache(true);
+
+  return {
+    presets: [
+      ['@babel/preset-env', {
+        targets: {
+          node: '12',
+        },
+      }],
+    ],
+  };
+};
