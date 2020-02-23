67499d4a134e2e8882dff26053a709de4b7f2f56
James Wright <james@jamesswright.co.uk>
2020-02-23 16:28:15 +0000

    Tweaks static server contents path

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,10 @@
-import nodeStatic from 'node-static';
 import { createServer } from 'http';
+import { resolve } from 'path';
+import nodeStatic from 'node-static';
 
 const PORT = '4002';
 
-const fileServer = new nodeStatic.Server('./app');
+const fileServer = new nodeStatic.Server(resolve(__dirname, '..', 'app'));
 
 const serveApp = () => {
   createServer((req, res) => {
